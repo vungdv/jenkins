198e71430a34ac30053944d82b967613f9b09408
vungdv <vungxd04@gmail.com>
2020-06-29 11:10:42 +0700

    remove cache

--- src/main/java/com/vungdo/demo/Calculator.java
+++ src/main/java/com/vungdo/demo/Calculator.java
@@ -1,8 +1,11 @@
 package com.vungdo.demo;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
+/** Calculator logic */
 @Service
 public class Calculator {
-int sum(int a, int b) {
+// @Cacheable("sum")
+public int sum(int a, int b) {
 return a + b;
 }
 }
