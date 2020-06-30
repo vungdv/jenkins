c99e6c37cac8d29a4df02a6be8b92de6767f2ead
vungdv <vungxd04@gmail.com>
2020-06-30 08:44:15 +0700

    fix test

--- acceptance_test.sh
+++ acceptance_test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 CALCULATOR_PORT=$(docker-compose port calculator 8080 | cut -d: -f2)
-test $(curl localhost:$CALCULATOR_PORT/sum?a=1\&b=2) -eq 3
+test $(curl http://localhost:$CALCULATOR_PORT/sum?a=1\&b=2) -eq 3
